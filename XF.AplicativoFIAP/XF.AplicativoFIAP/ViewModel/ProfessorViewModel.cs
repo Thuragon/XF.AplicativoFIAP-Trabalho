2c28790a42f4fc4662f4d8cab11384f20c40aae3
Logon Aluno <logonrmlocal@fiap.com.br>
2018-11-07 20:44:08 -0200

    Add project files.

---
+++ XF.AplicativoFIAP/XF.AplicativoFIAP/ViewModel/ProfessorViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using XF.AplicativoFIAP.Model;
+
+namespace XF.AplicativoFIAP.ViewModel
+{
+    public class ProfessorViewModel : INotifyPropertyChanged
+    {
+        #region Propriedades
+        public Professor ProfessorModel { get; set; }
+
+        public ProfessorViewModel()
+        {
+            
+
+        }
+
+
+        #endregion
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
